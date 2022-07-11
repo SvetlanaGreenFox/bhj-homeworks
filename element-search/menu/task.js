6a78037b3997717f12e76754d6f6e7b1d368d082
Светлана <svetlanagreenfox@gmail.com>
2022-07-11 17:53:32 +0300

    Corrected task menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,19 @@
-const menuItem = document.querySelectorAll('.menu__item');
+const subElements = document.querySelectorAll('.menu_sub');
 
-Array.from(menuItem).forEach((item) => {
-  item.addEventListener('click', (event) => {
-    const parent = item.closest('.menu');
-    event.preventDefault();
-    const innerMenu = item.querySelector('.menu_sub');
-    const activeItem = parent.querySelector('.menu_active');
+Array.from(subElements).forEach((elem) => {
+  const parentItem = elem.closest('.menu__item');
+  const parentMenu = elem.closest('.menu_main');
+
+  const titleLink = parentItem.querySelector('.menu__link');
+  titleLink.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const activeItem = parentMenu.querySelector('.menu_active');
     if (activeItem !== null) activeItem.classList.remove('menu_active');
-    if (innerMenu === null) return;
-    innerMenu.classList.add('menu_active');
+    if (activeItem === elem) {
+      elem.classList.remove('menu_active');
+      return;
+    }
+    elem.classList.add('menu_active');
   });
 });
